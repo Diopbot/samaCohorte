bc958f453d52afab928e895c5f79098e95e3fba8
Regulus <alassaneseck94@gmail.Com>
2021-05-16 13:16:35 +0000

    c'est mon 2nd commit

---
+++ fichier2.php
@@ -0,0 +1,17 @@
+<?php 
+$universite = 'Cheikh Anta Diop';
+echo( 'Master' .$universite );
+$filiere = 'Genie juridique';
+echo( 'Master 2' .$filiere );
+$specialite = ' Peche maritime';
+echo( 'Dakar' .$specialite);
+
+
+
+ 
+
+
+
+
+
+?>
