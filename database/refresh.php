0eae65f416b71e495ae1e9912a24f2716cb0feae
MbarodyYann <yanniang95@gmail.com>
2021-05-16 12:36:56 +0000

    Ajout du fichier de configuration de la bd

---
+++ database/refresh.php
@@ -0,0 +1,19 @@
+<?php
+
+$path = __DIR__ . '/database.sqlite';
+
+if (file_exists($path)){
+    unlink($path);
+}
+
+//$pdo = new PDO('sqlite:' . $path);
+$pdo = new PDO("sqlite:$path");
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$query = $pdo->prepare(
+    'CREATE TABLE posts ( 
+                    id INTEGER PRIMARY KEY,
+                    title VARCHAR(255) NOT NULL,
+                     body TEXT NOT NULL
+                )'
+);
+$query->execute();
