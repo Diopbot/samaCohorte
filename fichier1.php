b3596c98ac9319fbc6fb049eb5acfabebb27c26e
Regulus <alassaneseck94@gmail.Com>
2021-05-16 12:13:27 +0000

    ajout de ma villa

--- fichier1.php
+++ fichier1.php
@@ -4,5 +4,8 @@ $prenom = 'Fatou COLY';
 echo( 'Bonjour '.$prenom );
 $adresse = 'Parcelles Assainies';
 echo( ' Unite 25 ' .$adresse );
+$villa = 'Emergence' ;
+echo( ' numero 30 ' .$villa ); 
+
 
 ?>
