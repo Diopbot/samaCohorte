a22223097bdff7a8d6fe551be49444fc57e02226
MbarodyYann <yanniang95@gmail.com>
2021-05-16 12:47:32 +0000

    Affichage des posts sur la page

--- post.php
+++ post.php
@@ -12,6 +12,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST'){
 
     $query->execute([ $_POST['title'], $_POST['body'] ]);
 }
+if ($_SERVER['REQUEST_METHOD'] === 'GET'){
+    $path = __DIR__ . '/database/database.sqlite';
+    $pdo = new PDO('sqlite:'. $path);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+    $query = $pdo->prepare('SELECT * FROM posts');
+    $result = $query->execute();
+
+    $datas = $query->fetchAll();
+}
 
 ?>
 <!doctype html>
@@ -39,7 +50,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST'){
 
 <div>
     <h1>Liste des posts</h1>
-
+    <?php
+    if (empty($datas)){
+        echo('Nous n\'avons aucun post pour le moment');
+    }
+    ?>
+    <ul>
+        <?php foreach ($datas as $data): ?>
+            <li>
+                titre : <?= $data['title'] ?>
+                body : <?= $data['body'] ?>
+            </li>
+        <?php endforeach ?>
+    </ul>
 </div>
 
 </body>
