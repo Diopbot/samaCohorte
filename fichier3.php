064bb5d2a5fecbbc0617e3f83f67bcc91f4c9af5
Regulus <alassaneseck94@gmail.Com>
2021-05-16 14:36:35 +0000

    ajout de ma situation matrimoniale

---
+++ fichier3.php
@@ -0,0 +1,9 @@
+<?php 
+$nationalite = 'Senegalaise' ;
+echo('Femme' .$nationalite) ;
+$sitauationmatrimoniale = 'Celibataire' ;
+echo('vit seul' .$situationmatrimoniale) ;
+$nombredenfants = 'zero' ;
+echo('un parent vivant' .$nombredenfants) ;
+
+?>
